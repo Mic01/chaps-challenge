5e16aadccc9104eab615bc568d5d26dfc2445f3f
Justin Hanrahan <harnahjust@ecs.vuw.ac.nz>
2020-09-20 16:50:53 +1200

    finished loading of the first level

--- src/nz.ac.vuw.ecs.swen225.gp20/persistence/Persistence.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/persistence/Persistence.java
@@ -1,6 +1,10 @@
 package nz.ac.vuw.ecs.swen225.gp20.persistence;
 
-import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Tile;
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.maze.actors.Player;
+import nz.ac.vuw.ecs.swen225.gp20.maze.items.Key;
+import nz.ac.vuw.ecs.swen225.gp20.maze.items.Treasure;
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.*;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -23,39 +27,60 @@ public class Persistence {
      * @param fileName json file name
      * @return level tile array
      */
-    public static Tile[][] loadLevel(String fileName){
+    public static Tile[][] loadLevel(String fileName, Maze mazeObject){
 
-      Tile[][] maze;
+        //creates empty maze
+      Tile[][] maze = new Tile[80][80];
+      for(int i=0;i<80;i++){
+          for(int j=0;j<80;j++) {
+            maze[j][i] = new FreeTile();
+          }
+      }
 
         try {
             final JsonParser parser = Json.createParser(new FileReader("Assets/Level1.json"));
             while (parser.hasNext()) {
                 final Event event = parser.next();
                 if (event == Event.START_OBJECT) {
+
                     JsonObject obj = (JsonObject) parser.getObject();
                     final String tile = obj.getString("tile");
+
                     switch (tile) {
+
                         case "Wall":
-                            //make wall tile at obj.getInt("x") obj.getInt("y")
+                            maze[obj.getInt("x")][obj.getInt("y")] = new Wall();
                             break;
 
                         case "Floor":
-                            //make floor tile at obj.getInt("x") obj.getInt("y")
-                            //
+                            final String slot = obj.getString("slot");
+                            switch (slot) {
+                                case "Key":
+                                    maze[obj.getInt("x")][obj.getInt("y")] = new FreeTile(new Key(obj.getString("colour")));
+                                    break;
+                                case "Chip":
+                                    maze[obj.getInt("x")][obj.getInt("y")] = new FreeTile(new Treasure());
+                                    break;
+                                case "Player":
+                                    int x=obj.getInt("x");
+                                    int y=obj.getInt("y");
+                                    maze[x][y] = new FreeTile(new Player(x,y,mazeObject));
+                                    break;
+                            }
+                            break;
+
+                        case "Door":
+                            maze[obj.getInt("x")][obj.getInt("y")] = new LockedDoor(obj.getString("colour"));
                             break;
 
                         case "End":
-
+                            maze[obj.getInt("x")][obj.getInt("y")] = new Exit();
                             break;
 
                         case "Lock":
-
+                            maze[obj.getInt("x")][obj.getInt("y")] = new ExitLock(obj.getInt("chips"));
                             break;
                     }
-
-                    //  System.out.println(obj.getString("tile"));
-                    System.out.println(obj.getInt("x"));
-                    System.out.println(obj.getInt("y"));
                 }
             }
             parser.close();
@@ -63,5 +88,6 @@ public class Persistence {
         } catch (FileNotFoundException e){
             e.printStackTrace();
         }
+        return maze;
     }
 }
