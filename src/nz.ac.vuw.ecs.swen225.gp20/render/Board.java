add48c9c81a3704ca24fecfe1ec0bc7f1bb4ba94
Destiro <debruconn@ecs.vuw.ac.nz>
2020-09-23 23:17:51 +1200

    Restructured board class

--- src/nz.ac.vuw.ecs.swen225.gp20/render/Board.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/render/Board.java
@@ -1,52 +1,74 @@
 package nz.ac.vuw.ecs.swen225.gp20.render;
 
+import nz.ac.vuw.ecs.swen225.gp20.maze.Maze;
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.FreeTile;
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Ice;
 import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Tile;
 import nz.ac.vuw.ecs.swen225.gp20.maze.actors.Player;
 import nz.ac.vuw.ecs.swen225.gp20.maze.actors.Actor;
+import nz.ac.vuw.ecs.swen225.gp20.maze.tiles.Water;
 
-import java.awt.Dimension;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.io.IOException;
 import java.util.ArrayList;
 
 /**
  * Renderer class for displaying the board.
  */
-public class Board {
+public class Board extends JPanel {
 
   // Constant Variables
   public int visionRange = 9;
   public int reach = visionRange/2;
-  public int tileSize = 80; //Maybe change this
+  public int tileSize = 70;
+  public Dimension dimension = new Dimension(tileSize*visionRange, tileSize*visionRange);
 
   //Potential use
-  public boolean actorsPresent;
-  public int sleepTime = 200; //Time in ms before each draw
+  public int sleepTime = 100; //Time in ms before each draw (ill be adding half frames)
   public enum Soundeffects{
     metalWalk, waterSwim, iceWalk, lavaSwim, slide, pickup_item, finish_level, death, openDoor
   }
   public enum Animations{
-    walkLeft, walkRight, swimLeft, swimRight, doorOpen, death
+    doorOpen, death
   }
 
-  //Rendering Variables
-  public Dimension dimension = new Dimension(tileSize*visionRange, tileSize*visionRange);
+  //Rendering Variable
   public Tile[][] level;
+  public Tile[][] lastVision;
   public Tile[][] vision;
   public Player player;
-  public ArrayList<Actor> actors;
+  public Maze maze;
+  public ArrayList<Actor> moving = new ArrayList<>();
+  public ArrayList<String> animations = new ArrayList<>();
 
   /**
    * Construct a new Board when a new level is loaded.
    *
-   * @param level 2d array of full level
+   * @param maze Maze object, fetching values of current level
    * @param player current player
-   * @param actors Any actors on this level
    */
-  public Board(Tile[][] level, Player player, ArrayList<Actor> actors){
-    this.level = level;
+  public Board(Player player, Maze maze){
+    //JPanel Variables
+    this.setPreferredSize(dimension);
+    this.setBackground(new Color(255, 0, 255)); //for debugging
+    this.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+    //Setting Variables
+    this.maze = maze;
     this.player = player;
-    this.actors = actors;
     this.vision = new Tile[visionRange][visionRange];
-    if(actors.size() != 0){ actorsPresent = true; }
+    this.lastVision = new Tile[visionRange][visionRange];
+    updateLevel();
+  }
+
+  /**
+   * Fetches a new level from maze
+   * Called after a level is completed.
+   */
+  public void updateLevel(){
+    level = maze.getTiles();
+    setVision();
   }
 
   /**
@@ -54,6 +76,7 @@ public class Board {
    * of all tiles visible on board to draw.
    */
   public void setVision(){
+    lastVision = vision;
     int yCount=0;
     int xCount=0;
 
@@ -68,53 +91,103 @@ public class Board {
   }
 
   /**
-   * Draws the visible board and all entities on-top of tiles.
+   * Draws the visible board and all entities on-top of tiles,
+   * Calls the pre-built paint function of the JPanel and draws with graphics.
    *
-   * @param sound Play sound passed.
-   * @param animations Play a frame of each animation
+   * @param moving All moving chars, Draw a frame of each animation
+   * @param animations for non-moving animations
    */
-  public void draw(String sound, ArrayList<Actor> animations){
+  public void draw(ArrayList<Actor> moving, ArrayList<String> animations){
     setVision();
-    drawTiles();
-    drawEntities();
-    playSound(sound);
-    drawAnimations(animations);
+    this.moving = moving;
+    this.animations = animations;
+
+    this.repaint();
+    this.revalidate();
+  }
+
+  @Override
+  protected void paintComponent(Graphics g){
+    try {
+      drawTiles(g, 0);
+      drawEntities(g, 0);
+      drawAnimations();
+    } catch(IOException e){
+      e.printStackTrace();
+    }
   }
 
   /**
    * First step of draw method,
    * draws all tiles in players current vision.
    */
-  public void drawTiles(){
+  private void drawTiles(Graphics g, int offset) throws IOException{
     for(int xAxis=0; xAxis<visionRange; xAxis++){
       for(int yAxis=0; yAxis<visionRange; yAxis++){
-        //todo draw with passed canvas
-        //todo or by drawing onto positions of static canvas in maze
+        g.drawImage(vision[xAxis][yAxis].getImage(),
+                (xAxis*tileSize)+offset, (yAxis*tileSize)+offset, this);
       }
     }
   }
 
   /**
    * Second step of draw method,
-   * draws entities on-top of tiles (Actors, keys, etc...).
+   * draws a new frame of every actor that has moved this round.
    */
-  public void drawEntities(){
-    for(int xAxis=0; xAxis<visionRange; xAxis++) {
-      for(int yAxis=0; yAxis<visionRange; yAxis++){
-        //todo draw with passed canvas
-        //todo or by drawing onto positions of static canvas in maze
-      }
+  private void drawEntities(Graphics g, int offset) throws IOException {
+    for(Actor actor : moving){
+      g.drawImage(actor.getImage(),
+              (actor.getX()*tileSize)+offset, (actor.getY()*tileSize)+offset, this);
+
+      if(actor.getCurrentTile() instanceof FreeTile)
+        playSound("metalWalk");
+      else if(actor.getCurrentTile() instanceof Ice)
+        playSound("slide");
+      else if(actor.getCurrentTile() instanceof Water)
+        playSound("waterWalk");
     }
   }
 
   /**
    * Third step of draw method,
-   * Plays a sound from enum by fetching from Maze hashmap.
+   * Loops through unique (non-walk) animations and draws them.
+   */
+  private void drawAnimations(Graphics g){
+    for(String ani : animations){
+      playAnimations(ani, player, g);
+    }
+  }
+
+  /**
+   * Switch statement to draw a frame of an animation
+   * or create a new animation
+   *
+   * @param animation
+   */
+  public void playAnimations(String animation, Actor actor, Graphics g){
+    //todo create doorOpen and death myself
+    switch(Animations.valueOf(animation)){
+      case doorOpen:
+        playSound("openDoor");
+        break;
+
+      case death:
+        playSound("death");
+        break;
+
+      default:
+        throw new IllegalStateException("Unexpected value: " + Animations.valueOf(animation));
+    }
+  }
+
+  /**
+   * Plays a sound from assets folder
    *
    * @param sound of animation.
    */
   public void playSound(String sound){
-    //todo play sound files from michaiahs classes
+    //todo make sound files
+    //todo play them from here
     switch(Soundeffects.valueOf(sound)){
       case metalWalk:
         break;
@@ -145,44 +218,6 @@ public class Board {
 
       default:
         throw new IllegalStateException("Unexpected value: " + Soundeffects.valueOf(sound));
-    }
-  }
-
-  /**
-   * forth step of draw method,
-   * Animation of actors/player and soundeffects.
-   */
-  public void drawAnimations(ArrayList<Actor> animations){
-    for(Actor actor : animations){
-      //todo call playAnimations method
-      //todo base off of change in x/y
-    }
-  }
-
-  public void playAnimations(String animation){
-    //todo get walk/swim images from michaiahs classes
-    //todo create doorOpen and death myself
-    switch(Animations.valueOf(animation)){
-      case walkLeft:
-        break;
-
-      case walkRight:
-        break;
-
-      case swimLeft:
-        break;
-
-      case swimRight:
-        break;
-
-      case doorOpen:
-        break;
-
-      case death:
-        break;
-
-      default:
-        throw new IllegalStateException("Unexpected value: " + Animations.valueOf(animation));
     }
   }
 }
