0c6d117cbff3a76e62aec45a81020c224f7f9103
Michaiah Thoms <thomsmich@ecs.vuw.ac.nz>
2020-09-23 21:09:03 +1200

    Player now included in actor list in maze

--- src/nz.ac.vuw.ecs.swen225.gp20/maze/Maze.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/maze/Maze.java
@@ -8,7 +8,7 @@ import nz.ac.vuw.ecs.swen225.gp20.persistence.Persistence;
 
 public class Maze {
   private final Tile[][] tiles;
-  private final ArrayList<Actor> nonPlayerActors = new ArrayList<>();
+  private final ArrayList<Actor> actors = new ArrayList<>();
   private Player player;
 
   public Maze(String levelFile) {
@@ -46,22 +46,20 @@ public class Maze {
   }
 
   /**
-   * Get the list of actors that aren't controlled by the player.
+   * Get the list of actors in this maze.
    *
    * @return list of non player actors
    */
   public ArrayList<Actor> getActors() {
-    return nonPlayerActors;
+    return actors;
   }
 
   /**
-   * Add a non player actor to this maze.
+   * Add an actor to this maze.
    *
    * @param actor the actor to add to this maze
    */
   public void addActor(Actor actor) {
-    if (!(actor instanceof Player)) {
-      nonPlayerActors.add(actor);
-    }
+    actors.add(actor);
   }
 }
