4876315a35a5201a3b50a3bf7365145a67179301
Nicholas Woolf-Ben-Avraham <woolfnich@ecs.vuw.ac.nz>
2020-09-23 22:28:17 +1200

    Slight edit to Logo, fix Logo loading in SetupView

--- src/nz.ac.vuw.ecs.swen225.gp20/application/SetupView.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/application/SetupView.java
@@ -2,8 +2,6 @@ package nz.ac.vuw.ecs.swen225.gp20.application;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -12,7 +10,6 @@ public class SetupView {
     private JDialog dialog;
     private final CardLayout panes = new CardLayout();
     JPanel mainPanel;
-    private int i = 0;
 
     public SetupView(Main game) {
         this.game = game;
@@ -50,7 +47,7 @@ public class SetupView {
         titleConstraints.fill = GridBagConstraints.HORIZONTAL;
         titleConstraints.gridx = 0;
         titleConstraints.gridy = 0;
-        titleConstraints.insets = new Insets(-10, 0, 20, 0);
+        titleConstraints.insets = new Insets(0, 0, 10, 0);
         JLabel titleText = new JLabel("Team ____ presents:");
         titleText.setHorizontalAlignment(SwingConstants.CENTER);
         titleScreen.add(titleText, titleConstraints);
@@ -60,89 +57,28 @@ public class SetupView {
         titleConstraints.gridx = 0;
         titleConstraints.gridy = 1;
         titleConstraints.insets = new Insets(0, 0, 0, 0);
-        JLabel title = new JLabel(new ImageIcon(ClassLoader.getSystemResource("assets/Logo.png")));
+        JLabel title = new JLabel(new ImageIcon("assets/Logo.png"));
         titleScreen.add(title, titleConstraints);
 
         titleConstraints.anchor = GridBagConstraints.LINE_START;
         titleConstraints.fill = GridBagConstraints.NONE;
         titleConstraints.gridx = 0;
         titleConstraints.gridy = 2;
-        titleConstraints.insets = new Insets(40, 20, 0, 0);
+        titleConstraints.insets = new Insets(20, 20, 10, 0);
         JButton startGame = new JButton("Begin Game");
-        startGame.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                panes.next(mainPanel);
-            }
-        });
+        startGame.addActionListener(actionEvent -> panes.next(mainPanel));
         titleScreen.add(startGame, titleConstraints);
 
         titleConstraints.anchor = GridBagConstraints.LINE_END;
         titleConstraints.fill = GridBagConstraints.NONE;
         titleConstraints.gridx = 0;
         titleConstraints.gridy = 2;
-        titleConstraints.insets = new Insets(40, 0, 0, 20);
+        titleConstraints.insets = new Insets(20, 0, 10, 20);
         titleConstraints.ipadx = 55;
         JButton exit = new JButton("Exit");
-        exit.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                System.exit(0);
-            }
-        });
+        exit.addActionListener(actionEvent -> System.exit(0));
         titleScreen.add(exit, titleConstraints);
-        /**
-        //Panel for selecting number of players
-        JPanel panel1 = new JPanel(new GridBagLayout());
-        GridBagConstraints constraints = new GridBagConstraints();
 
-        constraints.anchor = GridBagConstraints.LINE_START;
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 0;
-        constraints.gridy = 0;
-        constraints.insets = new Insets(-148, 10, 20, 10);
-        panel1.add(new JLabel("Please input the number of players (minimum 3, maximum 6)."), constraints);
-
-        //JSpinner numOfPlayers = new JSpinner(this.playerLimit);
-        JTextField numOfPlayers = new JTextField(3);
-        constraints.fill = GridBagConstraints.NONE;
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.insets = new Insets(2, 100, 10, 10);
-        panel1.add(numOfPlayers, constraints);
-
-        JLabel errorMessage = new JLabel();
-        constraints.anchor = GridBagConstraints.CENTER;
-        errorMessage.setHorizontalAlignment(SwingConstants.CENTER);
-        constraints.fill = GridBagConstraints.HORIZONTAL;
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.insets = new Insets(0, 0, -150, 10);
-        panel1.add(errorMessage, constraints);
-
-        JButton next = new JButton("Next");
-        next.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                int playerCount;
-                try {
-                    playerCount = Integer.parseInt(numOfPlayers.getText().trim());
-                    if (playerCount < 3 || playerCount > 6) throw new ArrayIndexOutOfBoundsException("Out of range");
-                    panes.next(mainPanel);
-                } catch (NumberFormatException e) {
-                    errorMessage.setText("Please input a number.");
-                } catch (ArrayIndexOutOfBoundsException e) {
-                    errorMessage.setText("This number is invalid. Please try again.");
-                }
-            }
-        });
-        constraints.gridx = 0;
-        constraints.gridy = 1;
-        constraints.anchor = GridBagConstraints.LINE_END;
-        constraints.fill = GridBagConstraints.NONE;
-        constraints.insets = new Insets(0, 0, 10, 100);
-        panel1.add(next, constraints);
-        **/
         //Panel for level select
         JRadioButton l1, l2;
         ButtonGroup levels = new ButtonGroup();
@@ -176,24 +112,20 @@ public class SetupView {
 
 
         JButton next2 = new JButton("Next");
-        next2.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                ButtonModel model = levels.getSelection();
-                if (model == null) return;
-                switch (model.getActionCommand()) {
-                    case "1":
-                        //load level 1
-                        break;
-                    case "2":
-                        //load level 2
-                        break;
-                    //etc
-                    default:
-                }
-                dialog.dispose();
-                i++;
+        next2.addActionListener(actionEvent -> {
+            ButtonModel model = levels.getSelection();
+            if (model == null) return;
+            switch (model.getActionCommand()) {
+                case "1":
+                    //load level 1
+                    break;
+                case "2":
+                    //load level 2
+                    break;
+                //etc
+                default:
             }
+            dialog.dispose();
         });
         constraints2.gridx = 1;
         constraints2.gridy = 6;
