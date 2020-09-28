d6c26dc9d74e11ee5e608c84dcb9ecdea875df30
Michaiah Thoms <thomsmich@ecs.vuw.ac.nz>
2020-09-28 14:31:01 +1300

    Added getimages to items

--- src/nz.ac.vuw.ecs.swen225.gp20/maze/items/Item.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/maze/items/Item.java
@@ -1,4 +1,16 @@
 package nz.ac.vuw.ecs.swen225.gp20.maze.items;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 public abstract class Item {
+  protected static final String imageDirectory = "assets/tiles/";
+
+  /**
+   * Get this item's image.
+   *
+   * @return a BufferedImage of this item
+   * @throws IOException thrown if the file cannot be found for the item
+   */
+  public abstract BufferedImage getImage() throws IOException;
 }
