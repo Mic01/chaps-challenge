a9cf360c68fdd2c162b0137c5d83cb66ef0df1a5
Michaiah Thoms <thomsmich@ecs.vuw.ac.nz>
2020-09-28 15:23:10 +1300

    Nulltiles now get random empty images

--- src/nz.ac.vuw.ecs.swen225.gp20/maze/tiles/NullTile.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/maze/tiles/NullTile.java
@@ -4,9 +4,12 @@ import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
+import java.util.Random;
 import nz.ac.vuw.ecs.swen225.gp20.maze.actors.Actor;
 
 public class NullTile extends Tile {
+  private final static Random random = new Random();
+
   @Override
   public boolean isTraversable(Actor actor) {
     return false;
@@ -14,7 +17,7 @@ public class NullTile extends Tile {
 
   @Override
   public BufferedImage getImage() throws IOException {
-    return ImageIO.read(new File(imageDirectory + "empty.png"));
+    return ImageIO.read(new File(imageDirectory + "empty_" + random.nextInt(5) + ".png"));
   }
 
   @Override
