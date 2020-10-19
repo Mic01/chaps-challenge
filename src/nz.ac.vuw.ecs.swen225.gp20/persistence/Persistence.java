ea38e11d6ccd387bc334d83339f404cf70949316
Justin Hanrahan <hanrahjust@ecs.vuw.ac.nz>
2020-10-20 01:49:11 +1300

    Saving level 2 works

--- src/nz.ac.vuw.ecs.swen225.gp20/persistence/Persistence.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/persistence/Persistence.java
@@ -15,6 +15,7 @@ import javax.json.stream.JsonParser;
 import javax.json.stream.JsonParser.Event;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.lang.reflect.Array;
 import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
@@ -45,7 +46,6 @@ public class Persistence {
                     int x;
                     int y;
                     switch (tile) {
-
                         case "Inside":
                             for (int i = obj.getInt("y1"); i < obj.getInt("y2"); i++) {
                                 for (int j = obj.getInt("x1"); j < obj.getInt("x2"); j++) {
@@ -53,46 +53,32 @@ public class Persistence {
                                 }
                             }
                             break;
-
                         case "Wall":
                             maze[obj.getInt("x")][obj.getInt("y")] = new Wall();
                             break;
-
                         case "Floor":
+
                             final String slot = obj.getString("slot");
                             switch (slot) {
-                                case "Key":
-                                    maze[obj.getInt("x")][obj.getInt("y")] = new FreeTile(new Key(obj.getString("colour")));
+                                case "Item" :
+                                    maze[obj.getInt("x")][obj.getInt("y")] = new FreeTile(loadItem(obj));
                                     break;
-                                case "Chip":
-                                    maze[obj.getInt("x")][obj.getInt("y")] = new FreeTile(new Treasure());
-                                    break;
-                                case "Shoe":
-                                    final String element = obj.getString("type");
+
+                                case "Player":
                                     x = obj.getInt("x");
                                     y = obj.getInt("y");
-                                    System.out.println("Shoe");
-                                    switch (element) {
-
-                                        case "Water":
-                                            maze[x][y] = new FreeTile(new WaterPotion());
-                                        break;
-
-                                        case "Ice":
-                                            System.out.println("Ice");
-                                            maze[x][y] = new FreeTile(new IcePotion());
-                                        break;
-
+                                    Player player = new Player(x, y, mazeObject);
+                                    maze[x][y] = new FreeTile(player);
+                                    JsonArray inventory = obj.getJsonArray("inventory");
+                                    if(!inventory.isEmpty()){
+                                        for(JsonValue items: inventory){
+                                            System.out.println("Magic");
+                                            JsonObject item = (JsonObject) items;
+                                            player.pickup(loadItem(item));
+                                        }
                                     }
                                     break;
-                                case "Player":
-
-                                    x = obj.getInt("x");
-                                    y = obj.getInt("y");
-                                    maze[x][y] = new FreeTile(new Player(x, y, mazeObject));
-                                    break;
                                 case "Enemy":
-
                                     x = obj.getInt("x");
                                     y = obj.getInt("y");
                                     String direction = obj.getString("direction");
@@ -100,54 +86,42 @@ public class Persistence {
                                     switch (type) {
                                         case "One":
                                             maze[x][y] = new FreeTile(new EnemyOne(x, y, mazeObject, Actor.Direction.valueOf(direction)));
-
                                             break;
-
                                         case "Two":
                                             maze[x][y] = new FreeTile(new EnemyTwo(x, y, mazeObject,Actor.Direction.valueOf(direction)));
                                             break;
-
                                         case "Three":
                                             maze[x][y] = new FreeTile(new EnemyThree(x, y, mazeObject,Actor.Direction.valueOf(direction)));
                                             break;
                                     }
                                     break;
-
                                 case "Null":
                                     maze[obj.getInt("x")][obj.getInt("y")] = new FreeTile();
                                     break;
                             }
                             break;
-
                         case "Door":
                             maze[obj.getInt("x")][obj.getInt("y")] = new LockedDoor(obj.getString("colour"), obj.getBoolean("vertical"), obj.getBoolean("open"));
                             break;
-
                         case "End":
                             maze[obj.getInt("x")][obj.getInt("y")] = new Exit();
                             break;
-
                         case "Lock":
                             maze[obj.getInt("x")][obj.getInt("y")] = new ExitLock(obj.getInt("chips"), obj.getBoolean("vertical"), obj.getBoolean("open"));
+                            mazeObject.setTreasuresLeft(obj.getInt("chips"));
                             break;
-
                         case "Info":
                             maze[obj.getInt("x")][obj.getInt("y")] = new InfoTile(obj.getString("text"));
                             break;
-
                         case "Conveyor":
-
                             maze[obj.getInt("x")][obj.getInt("y")] = new Conveyor(Actor.Direction.valueOf(obj.getString("direction")));
                             break;
-
                         case "Ice":
                             maze[obj.getInt("x")][obj.getInt("y")] = new Ice();
                             break;
-
                         case "Water":
                             maze[obj.getInt("x")][obj.getInt("y")] = new Water();
                             break;
-
                         case "Vent":
                             maze[obj.getInt("x")][obj.getInt("y")] = new Vent(obj.getInt("x1"),obj.getInt("y1"));
                             break;
@@ -155,15 +129,35 @@ public class Persistence {
                 }
             }
             parser.close();
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
-
-        //saveLevel(maze, "assets/SaveFile.json");
-
+        saveLevel(maze, "levels/SaveFile.json");
         return maze;
     }
+
+    public static Item loadItem(JsonObject obj){
+        System.out.println(obj.getString("type"));
+        final String type = obj.getString("type");
+
+        switch (type) {
+            case "Key" :
+                return new Key(obj.getString("colour"));
+            case "Chip" :
+                return new Treasure();
+            case "Shoe" :
+                final String element = obj.getString("element");
+                switch (element) {
+                    case "Water":
+                        return new WaterPotion();
+                    case "Ice":
+                        return new IcePotion();
+                }
+                break;
+        }
+        return null;
+    }
+
 
     /**
      * coverts a 2D of Tiles into json and saves it
@@ -186,8 +180,6 @@ public class Persistence {
                 Tile currentTile = maze[i][j];
                 JsonObjectBuilder object = Json.createObjectBuilder();
 
-
-
                 if (currentTile instanceof NullTile) {
                     continue;
                 }
@@ -202,18 +194,32 @@ public class Persistence {
                     object.add("vertical", ((LockedDoor) currentTile).isVertical());
                     object.add("open", ((LockedDoor) currentTile).isOpen());
                 } else if (currentTile instanceof FreeTile) {
-                    if (((FreeTile) currentTile).hasActor()) {
-                        object.add("slot", "Player");
-                       // object.add("inventory", ((FreeTile) currentTile).getActor().getInventory());
 
-                    } else if (((FreeTile) currentTile).hasItem()) {
-                        Item item = ((FreeTile) currentTile).getItem();
-                        if (item instanceof Key) {
-                            object.add("slot", "Key");
-                            object.add("colour", ((Key) item).getColour());
-                        } else if (item instanceof Treasure) {
-                            object.add("slot", "Chip");
+                    if (((FreeTile) currentTile).hasActor()) {
+                        Actor actor = ((FreeTile) currentTile).getActor();
+                        if(actor instanceof Player){
+                            object.add("slot", "Player");
+                            JsonArrayBuilder array = Json.createArrayBuilder();
+                            for(Item item: ((Player)actor).getInventory()){
+                                JsonObjectBuilder itemObject = Json.createObjectBuilder();
+                                saveItem(itemObject, item);
+                                array.add(itemObject);
+                            }
+                            object.add("inventory", array);
+                        }else{
+                            object.add("slot", "Enemy");
+                            //object.add("direction", ((AutoActor)actor.getCurrentDirection()));
+                            object.add("direction", "left");
+                            if(actor instanceof EnemyOne){
+                                object.add("type", "One");
+                            }else if(actor instanceof EnemyTwo){
+                                object.add("type", "Two");
+                            } else if(actor instanceof EnemyThree){
+                                object.add("type", "Three");
+                            }
                         }
+                    } else if (((FreeTile) currentTile).hasItem()) {
+                        saveItem(object, ((FreeTile) currentTile).getItem());
                     } else{
                         object.add("slot", "Null");
 
@@ -221,21 +227,25 @@ public class Persistence {
                 }else if (currentTile instanceof InfoTile){
                     object.add("text", ((InfoTile)currentTile).getInfo());
 
+                }else if (currentTile instanceof Vent){
+                   object.add("x1", ((Vent)currentTile).getTargetX());
+                    object.add("y1", ((Vent)currentTile).getTargetY());
+
+                }else if (currentTile instanceof Conveyor){
+                    //object.add("direction", ((Conveyor)currentTile).getDirection);
+                    object.add("direction", "up");
                 }
 
                 object.add("x", i);
                 object.add("y", j);
-
                 JsonObject builtObject = object.build();
                 output.append(builtObject.toString());
-
                 output.append(",");
             }
         }
 
         output.deleteCharAt(output.length()-1);
         output.append("]");
-
         try {
             FileWriter file = new FileWriter(fileName);
             file.write(output.toString());
@@ -244,6 +254,23 @@ public class Persistence {
         }  catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Item saveItem(JsonObjectBuilder object, Item item) {
+        object.add("slot", "Item");
+        if (item instanceof Key) {
+            object.add("type", "Key");
+            object.add("colour", ((Key) item).getColour());
+        } else if (item instanceof Treasure) {
+            object.add("type", "Chip");
+        } else if (item instanceof IcePotion) {
+            object.add("type", "Shoe");
+            object.add("element", "Ice");
+        }else if (item instanceof WaterPotion) {
+            object.add("type", "Shoe");
+            object.add("element", "Water");
+        }
+        return null;
     }
 }
 
