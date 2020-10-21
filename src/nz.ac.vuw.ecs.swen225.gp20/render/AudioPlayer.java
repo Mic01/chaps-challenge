2afae99fb80a3eb2b7a6e5dd6e4a105b309a8bd6
Destiro <debruconn@ecs.vuw.ac.nz>
2020-10-21 13:07:33 +1300

    Checkstyles and updated animatespeed method

--- src/nz.ac.vuw.ecs.swen225.gp20/render/AudioPlayer.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/render/AudioPlayer.java
@@ -4,19 +4,35 @@ import javax.sound.sampled.Clip;
 import javax.sound.sampled.LineEvent;
 import javax.sound.sampled.LineListener;
 
+/**
+ * Audio Player class that acts like a "boombox"
+ * Plays one audio at a time dependant on their priority rating.
+ */
 public class AudioPlayer implements LineListener {
   private Clip currentClip;
   private boolean finishedPlay = true;
   private int currentPriority;
 
+  /**
+   * Checks whether the passed audio can play,
+   * based on if theres no audio playing or its priority,
+   * plays audio if able to.
+   *
+   * @param priority of clip, to override other sounds
+   * @param soundEffect object to retrieve clip
+   */
   public void playAudio(int priority, SoundEffect soundEffect) {
-    if(priority > currentPriority){
+    //New audio should be played, stop current audio
+    if (priority > currentPriority) {
       finishedPlay = true;
-      if(currentClip != null) currentClip.stop();
+      if (currentClip != null) {
+        currentClip.stop();
+      }
       currentPriority = priority;
     }
 
-    if(finishedPlay) {
+    //Audio finished playing, play new audio
+    if (finishedPlay) {
       currentClip = soundEffect.getClip();
       currentClip.addLineListener(this);
       currentClip.setFramePosition(0);
@@ -24,7 +40,7 @@ public class AudioPlayer implements LineListener {
     }
 
     //Allows pickup item to override other pickup items
-    if(soundEffect.getName().equals("pickup_item")) {
+    if (soundEffect.getName().equals("pickup_item")) {
       currentPriority = 2;
     }
   }
@@ -33,9 +49,9 @@ public class AudioPlayer implements LineListener {
   public void update(LineEvent event) {
     LineEvent.Type type = event.getType();
 
-    if(type == LineEvent.Type.START){
+    if (type == LineEvent.Type.START) {
       finishedPlay = false;
-    } else if(type == LineEvent.Type.STOP){
+    } else if (type == LineEvent.Type.STOP) {
       finishedPlay = true;
       currentPriority = 1;
       currentClip.stop();
--- src/nz.ac.vuw.ecs.swen225.gp20/render/SoundEffect.java
+++ src/nz.ac.vuw.ecs.swen225.gp20/render/SoundEffect.java
@@ -1,14 +1,26 @@
 package nz.ac.vuw.ecs.swen225.gp20.render;
 
 import java.io.File;
+import javax.sound.sampled.AudioFormat;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.DataLine;
 
-import javax.sound.sampled.*;
-
+/**
+ * SoundEffect class to construct clips
+ * for sound playing in the Audio player.
+ */
 public class SoundEffect {
   private Clip clip;
-  private String name;
+  private final String name;
 
-  public SoundEffect(String fileName){
+  /**
+   * Generates clip from Audio stream by loading the file.
+   *
+   * @param fileName of the sound file
+   */
+  public SoundEffect(String fileName) {
     name = fileName;
     try {
       AudioInputStream audioStream = AudioSystem.getAudioInputStream(
@@ -17,12 +29,26 @@ public class SoundEffect {
       DataLine.Info info = new DataLine.Info(Clip.class, format);
       clip = (Clip) AudioSystem.getLine(info);
       clip.open(audioStream);
-    }catch(Exception e){ e.printStackTrace(); }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
   }
 
-  public Clip getClip(){
+  /**
+   * Returns clip of the SoundEffect.
+   *
+   * @return clip
+   */
+  public Clip getClip() {
     return clip;
   }
 
-  public String getName(){ return name; }
+  /**
+   * Returns the name of the SoundEffect.
+   *
+   * @return name
+   */
+  public String getName() {
+    return name;
+  }
 }
